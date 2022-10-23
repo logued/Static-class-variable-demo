37aac0f7c40d4d5c151686a62d513074ab50c7f7
dermo <dermot.logue@dkit.ie>
2022-10-23 20:38:53 +0100

    comment updates

--- src/main/java/org/example/Dog.java
+++ src/main/java/org/example/Dog.java
@@ -1,2 +1,37 @@
-package org.example;public class Dog {
+package org.example;
+
+/**
+ * Demonstrates use of a "class variable", which is a variable that is declared
+ * using the "static" keyword.  e.g. "static int count"
+ * Static variables belong to the class, and are NOT created as fields in each object when
+ * we instantiate the class. So, when we create Dog objects below, the Dog objects will
+ * NOT have count instance fields in them.  Instead, there is only one variable called
+ * count, and it belongs to the class. The class exists as a structure in its own right,
+ * independent of the objects (instances) that are created based on the class.
+ *
+ * We can use the static count variable to count the number of Dog objects created by
+ * incrementing the count by one each time the Dog constructor is called.
+ *
+ */
+
+public class Dog {
+
+    // instance variables appear in every instance of a class
+    private String name;
+    private int age;
+    // a static class variable exists only once in the class, but NOT in th objects created
+    private static int count=0; // class variable used to count the number of instances created
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+        count++;        // add 1 to static class variable each time a Dog object is constructed
+    }
+
+    // We create a static method to access the static variable count.
+    // The static method belongs to the class, so we have to call the method
+    // using the syntax:  ClassName.methodName()    (See the call in main())
+    public static int getCount() {
+        return count;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,22 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Dog object Counter - using static variable");
+
+        // create two instances of the Dog class (i.e. two objects)
+        Dog dog1 = new Dog("Spot",6);
+        Dog dog2 = new Dog("Lassie",7);
+
+        // To access a class method (static method we use the following syntax:
+        //  ClassName.methodName,   so here, Dog.getCount() accesses the static method.
+
+        System.out.println("Number of Dog objects = " + Dog.getCount());
+
+        // We can NOT call getCount() on a Dog instance (object)
+        // i.e.  dog1.getCount() - is an ERROR
+        // because, the getCount method does not exist for an object,
+        // it exists only for the class.
+
     }
 }
